228e5187db9cbe3332aebd123f68cfa52cfda519
tanasak32123 <tanasak32123@gmail.com>
2022-12-11 21:18:09 +0700

    last updated

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,15 +7,17 @@ class MainController < ApplicationController
   end
 
   def create
-    user = User.where(email: params[:email]).first
-    if user && user.authenticate(params[:password])
-      session[:logged_in] = true
-      session[:userId] = user.id
-      session[:userEmail] = user.email
-      session[:user_type] = user.user_type
-      redirect_to main_main_path
-    else
-      redirect_to main_login_path, alert: 'Wrong username or password.' and return 
+    User.transaction do
+      user = User.where(email: params[:email]).first
+      if user && user.authenticate(params[:password])
+        session[:logged_in] = true
+        session[:userId] = user.id
+        session[:userEmail] = user.email
+        session[:user_type] = user.user_type
+        redirect_to main_main_path
+      else
+        redirect_to main_login_path, alert: 'Wrong username or password.' and return 
+      end
     end
   end
 
@@ -42,30 +44,34 @@ class MainController < ApplicationController
   end
 
   def update_profile
-    user = User.where(email: params[:email]).first
-    if user
-      redirect_to new_profile_path(name: params[:name]), alert: 'This email is already used.'
-    else
-      user = User.where(id: session["userId"]).first
+    User.transaction do
+      user = User.where(email: params[:email]).first
       if user
-        user.update(email: params[:email], name: params[:name], user_type: params[:role].to_i)
-        session[:userEmail] = user.email
-        session[:user_type] = user.user_type
-        redirect_to main_profile_path, :flash => { :success => "Update your profile successfully." }
+        redirect_to new_profile_path(name: params[:name]), alert: 'This email is already used.'
       else
-        redirect_to new_profile_path, :flash => { :alert => "Cannot change your profile. Please try again." } 
+        user = User.where(id: session["userId"]).first
+        if user
+          user.update(email: params[:email], name: params[:name], user_type: params[:role].to_i)
+          session[:userEmail] = user.email
+          session[:user_type] = user.user_type
+          redirect_to main_profile_path, :flash => { :success => "Update your profile successfully." }
+        else
+          redirect_to new_profile_path, :flash => { :alert => "Cannot change your profile. Please try again." } 
+        end
       end
     end
   end
 
   def update_password
-    user = User.where(id: session["userId"]).first
-    if user
-      user.password = params[:newPassword]
-      user.save
-      redirect_to main_profile_path, :flash => { :success => "Change password successfully." }
-    else
-      redirect_to new_password_path, :flash => { :alert => "Cannot change your password. Please try agian." } 
+    User.transaction do
+      user = User.where(id: session["userId"]).first
+      if user
+        user.password = params[:newPassword]
+        user.save
+        redirect_to main_profile_path, :flash => { :success => "Change password successfully." }
+      else
+        redirect_to new_password_path, :flash => { :alert => "Cannot change your password. Please try agian." } 
+      end
     end
   end
 
@@ -75,12 +81,14 @@ class MainController < ApplicationController
   end
 
   def create_user
-    user = User.where(email: params[:email]).first
-    if user
-      redirect_to new_account_path(name: params[:name]), alert: 'This email is already used.'
-    else
-      User.create(email: params[:email], name: params[:name], password: params[:password], user_type: params[:role].to_i)
-      redirect_to main_login_path, :flash => { :success => "Your account has been created." }
+    User.transaction do
+      user = User.where(email: params[:email]).first
+      if user
+        redirect_to new_account_path(name: params[:name]), alert: 'This email is already used.'
+      else
+        User.create(email: params[:email], name: params[:name], password: params[:password], user_type: params[:role].to_i)
+        redirect_to main_login_path, :flash => { :success => "Your account has been created." }
+      end
     end
   end
 
